2162e1434fc13fb8b3f36f70187ffad2b21b44c3
fabiolmv <fabiolmv@gmail.com>
2019-09-13 08:34:02 -0300

    Criação das Endidades

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using QuickBuy.Dominio.ObjetoDeValor;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime DataPedido { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataPrevisaoEntrega { get; set; }
+
+        public string Cep { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string EnderecoCompleto { get; set; }
+        public int NumeroEndereco { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento { get; set; }
+
+
+        /// <summary>
+        /// Pedido pode ter pelo menos um item de pedido ou muitos itens pedidos
+        /// </summary>
+        public ICollection<ItemPedido> ItensPedidos { get; set; }
+    }
+}
